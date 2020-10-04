fd2da5712d650308c08db43357253538b9a40e90
Dmytro Honcharov <goncharovdma@gmail.com>
2020-10-05 00:26:39 +0300

    add offer{n} test

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
+    PRODUCT_NAME = (By.XPATH, '//div[contains(@class, "product_main")]/h1')
+    PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]/p[@class="price_color"]')
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,54 @@
+import math
+
+from selenium.common.exceptions import NoAlertPresentException
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def should_be_able_to_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), \
+            "Item should be able to added to the cart"
+        assert self.browser.find_element(
+            *ProductPageLocators.ADD_TO_BASKET_BUTTON).text == "Add to basket", \
+            "Text on button should be Add to basket"
+
+    def test_product_and_price(self):
+        assert self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME).text == "Coders at Work", \
+            "The name of product should be Coders at Work"
+        assert self.browser.find_element(
+            *ProductPageLocators.PRICE).text == "£19.99", \
+            "The price of product is 19.99£"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_be_present_in_cart(self) -> None:
+        assert self.is_element_present(
+            *ProductPageLocators.PRODUCT_NAME), "Product name is not present"
+        assert self.is_element_present(
+            *ProductPageLocators.ALERT_ADDED_TO_CART
+        ), "No alert that a product has been added to cart"
+        alert_text = self.browser.find_element(
+            *ProductPageLocators.ALERT_ADDED_TO_CART).text
+        product_name = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME).text
+        assert product_name == alert_text, \
+            f"The alert contains wrong product name: {alert_text} - {product_name}"
